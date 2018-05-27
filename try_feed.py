cb4d8d30f6dbed342adc4e915623d0933bf9d950
ihfazhillah <mihfazhillah@gmail.com>
2018-05-27 11:37:45 +0700

    make it suitable for heroku deployment

--- try_feed.py
+++ try_feed.py
@@ -1,45 +1,26 @@
-import requests
+import html
+import feedparser
 import mongoengine
 import telethon
 import time
 import os
-import sys
+
+
 
 # constants
-HOST = os.environ.get('MONGODB_HOST')
-DATABASE = os.environ.get('MONGODB_DB', 'upwork')
-CHANNEL = os.environ.get('TG_CHANNEL')
-MYUSERNAME = os.environ.get('MYUSERNAME', 'IbnAmin')
 
-API_ID = os.environ.get('API_ID') 
-API_HASH = os.environ.get('API_HASH')
+MONGODB_NAME = "upwork"
+MONGODB_HOST = os.environ.get("MONGODB_HOST")
 
-if not HOST:
-    print('please provide the MONGODB_HOST environment variable as mongodb host string')
-    sys.exit()
+API_ID = os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
 
-if not CHANNEL:
-    print('please provide the TG_CHANNEL environment variable ')
-    sys.exit()
-
-if not API_ID:
-    print('please provide the API_ID environment variable ')
-    sys.exit()
-
-if not API_HASH:
-    print('please provide the API_HASH environment variable ')
-    sys.exit()
-
-RSS2JSONBASE = "https://api.rss2json.com/v1/api.json?rss_url="
+USERNAME = os.environ.get("USERNAME")
+CHANNEL_URL = os.environ.get("CHANNEL_URL")
 
 
+mongoengine.connect(MONGODB_NAME, host=MONGODB_HOST)
 
-mongoengine.connect(DATABASE, host=HOST)
-
-t_client = telethon.TelegramClient('upwork-rss', API_ID, API_HASH )
-t_client.start()
-
-channel = t_client.get_entity(CHANNEL)
 
 class Url(mongoengine.Document):
     title = mongoengine.StringField()
@@ -54,32 +35,45 @@ class Item(mongoengine.Document):
         return self.title
 
 while True:
+    t_client = telethon.TelegramClient('session', API_ID, API_HASH )
+    t_client.start()
+
+    channel = t_client.get_entity(CHANNEL_URL)
+
     for url in Url.objects:
         print("processing " + url.title)
-        t_client.send_message(MYUSERNAME, "ini sedang memulai proses untuk " + url.title)
-        to_get = RSS2JSONBASE + url.url
-        resp = requests.get(to_get)
+        resp = feedparser.parse(url.url)
+        resp.entries.reverse()
+
+
 
         # loop through items
         try:
-            for item in resp.json()['items']:
-                found = Item.objects(guid=item['guid']).first()
+            for item in resp.entries:
+                found = Item.objects(guid=item.guid)
 
-                if not found:
-                    to_save = Item(title=item['title'], guid=item['guid'], description=item['description'])
+
+                if found.count() == 0:
+                    to_save = Item(title=item.title,
+                                   guid=item.guid,
+                                   description=item.summary
+                                   )
                     to_save.save()
-                    message = "<b>" + to_save.title + "</b>\n\n\n"
-                    message += to_save.description.replace("<br>", "\n").replace("<br/>", "\n")
 
-                    t_client.send_message(channel, message, parse_mode="html", link_preview=False)
+                    message = "###################\n\n\n"
+                    message += "<b>" + to_save.title + "</b>\n\n\n"
+                    message += to_save.description.replace("<br>", "\n").replace("<br/>", "\n").replace("<br />", "\n").replace("<br >", "\n")
+                    message += "##################"
+
+                    t_client.send_message(channel, html.unescape(message), parse_mode="html", link_preview=False)
+                    t_client.send_message(channel, "#---> " + url.title)
                     time.sleep(5)
         except KeyError:
-            t_client.send_message(MYUSERNAME, "ada error di url " + url.url)
-            t_client.send_message(MYUSERNAME, "please check")
-            t_client.send_message(MYUSERNAME, "```" + resp.content + "```")
+            t_client.send_message(USERNAME, "ada error di url " + url.url)
+            t_client.send_message(USERNAME, "please check")
+            t_client.send_message(USERNAME, "```" + resp.content + "```")
         except Exception as e:
-            t_client.send_message(MYUSERNAME, "another error, please check the script")
-            t_client.send_message(MYUSERNAME, "```" + str(e) + "```")
+            t_client.send_message(USERNAME, "another error, please check the script")
+            t_client.send_message(USERNAME, "```" + str(e) + "```")
 
     time.sleep(10 * 60)
-
